d2057c46b56492ca4850e174d61e0895c3f5c508
agayayav <aina.gayayavila@bsc.es>
2024-01-15 19:58:41 +0100

    final

--- run_sierpinsky.sh
+++ run_sierpinsky.sh
@@ -4,9 +4,9 @@ iterations=$1
 
 echo $iterations > tmp.input
 
-#gfortran sierpinski.F90 -o sierpinski_gen.out
+gfortran sierpinski.F90 -o sierpinski_gen.out
 
-#./sierpinski_gen.out < tmp.input
+./sierpinski_gen.out < tmp.input
 
 python3 visualize_sierpinski.py -k ${iterations}
 
